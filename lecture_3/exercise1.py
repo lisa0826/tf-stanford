f932d94902cfcb60399182d88966cc6584ac1f5c
lisa_qiu <lisa_qiu@qq.com>
2019-01-18 23:13:07 +0800

    3-1
    tensorboard

--- lecture_3/exercise1.py
+++ lecture_3/exercise1.py
@@ -9,3 +9,12 @@ pow_op = tf.pow(add_op, mul_op)
 with tf.Session() as sess:
 	z = sess.run(pow_op)
 	print(z)
+
+
+#Interactive Coding       交互式编码
+#Mean squared error       均方误差
+#Specify loss function    指定损失函数
+#Initialize variables     初始化变量
+#Fetch argument           获取参数
+#Huber loss               湖贝儿损失
+#Placeholder              占位符
