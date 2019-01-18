63f38ab8b3065fcc67aa226f6f088bf05a3320e6
lisa <lisa_qiu@qq.com>
2019-01-18 10:43:15 +0800

    第二课第二个练习添加注释

--- lecture_2/exercise2.py
+++ lecture_2/exercise2.py
@@ -1,5 +1,7 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+#用于隐藏这个提示”Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX2 FMA“
+
 import tensorflow as tf
 
 a = tf.constant(2)
